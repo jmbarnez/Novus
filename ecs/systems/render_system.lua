6270dafd5646b13731809f05e173ba62d0044c9c
Jesse Barnes <bjesse14@gmail.com>
2025-12-19 12:44:57 -0500

    add asteroid outline rendering for selected/hovered targets, remove unused target panel widget

--- ecs/systems/render_system.lua
+++ ecs/systems/render_system.lua
@@ -10,6 +10,50 @@ local function lerpAngle(a, b, t)
   return a + d * t
 end
 
+local function buildExpandedOutlineCoords(baseCoords, pad)
+  local coords = {}
+  for i = 1, #baseCoords, 2 do
+    local x = baseCoords[i]
+    local y = baseCoords[i + 1]
+    local len = math.sqrt(x * x + y * y)
+    if len > 0.0001 then
+      local s = (len + pad) / len
+      coords[i] = x * s
+      coords[i + 1] = y * s
+    else
+      coords[i] = x
+      coords[i + 1] = y
+    end
+  end
+  return coords
+end
+
+local function getAsteroidTargetOutlineCoords(e, shape, pad)
+  local a = e.asteroid
+  if not a then
+    return nil
+  end
+
+  local baseCoords
+  if a.renderCoords then
+    baseCoords = a.renderCoords
+  else
+    if not a._physicsRenderCoords then
+      a._physicsRenderCoords = { shape:getPoints() }
+    end
+    baseCoords = a._physicsRenderCoords
+  end
+
+  if a._targetOutlineCoords and a._targetOutlinePad == pad and a._targetOutlineBase == baseCoords then
+    return a._targetOutlineCoords
+  end
+
+  a._targetOutlineCoords = buildExpandedOutlineCoords(baseCoords, pad)
+  a._targetOutlinePad = pad
+  a._targetOutlineBase = baseCoords
+  return a._targetOutlineCoords
+end
+
 local RenderSystem = Concord.system({
   renderables = { "physics_body", "renderable" },
 })
@@ -242,7 +286,6 @@ function RenderSystem:drawWorld()
         love.graphics.rotate(angle)
 
         if e == selected or e == hovered then
-          local r = (e.asteroid and e.asteroid.radius) or 30
           love.graphics.push("all")
           if e == selected then
             love.graphics.setColor(1.00, 0.35, 0.95, 0.55)
@@ -251,7 +294,14 @@ function RenderSystem:drawWorld()
             love.graphics.setColor(0.00, 1.00, 1.00, 0.40)
             love.graphics.setLineWidth(2)
           end
-          love.graphics.circle("line", 0, 0, r + 10)
+
+          local outline = getAsteroidTargetOutlineCoords(e, shape, 10)
+          if outline then
+            love.graphics.polygon("line", outline)
+          else
+            local r = (e.asteroid and e.asteroid.radius) or 30
+            love.graphics.circle("line", 0, 0, r + 10)
+          end
           love.graphics.pop()
         end
 
